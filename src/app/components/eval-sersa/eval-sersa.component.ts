897e15494aac473024928fdb5195e851aa607e8c
Maria <marilau63@gmail.com>
2018-10-09 19:25:05 -0600

    ya se actualiza el formulario en la base

--- src/app/components/eval-sersa/eval-sersa.component.ts
+++ src/app/components/eval-sersa/eval-sersa.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { Router } from '@angular/router';
 
+
 import 'rxjs/add/operator/switchMap';
 
 import { GetEvalSersaService } from './get-eval-sersa.service'
@@ -11,19 +12,27 @@ import { Infrastructure  } from '../../common/model/Infrastructure'
 import { Evaluation } from '../../common/model/Evaluation'
 import { UserService } from "app/common/service/user.service";
 import { AngularFire } from "angularfire2";
+import {Location} from '@angular/common';
+import { AngularFireService } from '../../common/service/angularFire.service'; 
+import { TomaDatos } from '../../common/model/TomaDatos';
 
 @Component({
   selector: 'app-eval-sersa',
   templateUrl: './eval-sersa.component.html',
   styleUrls: ['./eval-sersa.component.scss'],
-  providers: [GetEvalSersaService, ToasterService, UserService]
+  providers: [GetEvalSersaService, ToasterService, AngularFireService, UserService]
 })
 export class EvalSersaComponent implements OnInit, OnDestroy {
   private sub: any;
-  private infrastructureId: string;
+  private tomaDatosId: string;
   public form: FormularioSersa[];
   private answers: Boolean[];
   private type: string;
+  riesgo_name:string;
+  riesgo_num;
+  idToma;
+  idInfra;
+  tomaDatosInfra: TomaDatos;
 
   /*			Toast variables		*/
   public toastConfig: ToasterConfig = new ToasterConfig({
@@ -31,21 +40,40 @@ export class EvalSersaComponent implements OnInit, OnDestroy {
     limit: 5
   });
 
-  constructor(private route: ActivatedRoute,
+  constructor(private _location: Location, private route: ActivatedRoute,
     private getFormService: GetEvalSersaService,
+    private angularFireService: AngularFireService,
     private router: Router,
     private toasterService: ToasterService,
     private userService: UserService,
-    private af: AngularFire) { }
+    private af: AngularFire
+  ) { }
   private user;
 
   ngOnInit() {
+    //this.riesgo_name='hola';
     this.loadUserPermissions();
     this.sub = this.route.params.subscribe((params: Params) => {
-      this.infrastructureId = params['id'];
+      this.tomaDatosId = params['id'];
       this.type = params['type'];
+      this.idInfra = params['idInfra'];
     });
     this.getForm();
+    console.log(this.idInfra);
+
+    this.angularFireService.getTomaDatos(this.tomaDatosId).subscribe(
+      results => {
+        this.tomaDatosInfra = results;
+        for (var i = 0; i<(results.infraestructuras).length;i++){
+          if(results.infraestructuras[i]['id'] == this.idInfra){
+            this.idToma = i;
+          }
+        }
+
+        console.log(results.infraestructuras[this.idToma]);
+      });
+
+    
   }
   loadUserPermissions() {
     this.user = {};
@@ -71,26 +99,45 @@ export class EvalSersaComponent implements OnInit, OnDestroy {
       }
       );
   }
+
+  nombreRiesgo(){
+    if(this.riesgo_num == 0)
+    this.riesgo_name = "Nulo";
+  else if(this.riesgo_num < 3)
+    this.riesgo_name = "Bajo";
+  else if(this.riesgo_num < 5)
+    this.riesgo_name = "Intermedio";
+  else if(this.riesgo_num < 8)
+    this.riesgo_name = "Alto";
+  else
+    this.riesgo_name = "Muy Alto";
+
+    return this.riesgo_name;
+
+
+  }
+
   saveAnswers() {
     if (this.arrayComplete(this.answers)) {
+      
       let risk: number = this.calculateRisk();
-      const infrastructure = this.getFormService.getInfrastructure(this.infrastructureId);
-      this.pushAnswers(risk);
-      infrastructure.update({ risk: risk });
-
-      this.popSuccessToast("Se guardo la evaluacion.");
-      setTimeout(() => {
-        this.goBack();
-      },
-        2000);
+      console.log("riesgo" + risk);
+      
+      //this.riesgo_name = "adios";
+      
+      this.riesgo_num = risk;
+        
+  
+        document.getElementById("openModalButton").click();
+    
+      
 
     } else {
       this.popErrorToast("El formulario esta incompleto.");
     }
 
-
-
   }
+
   calculateRisk() {
     let risk: number = 0;
     for (let answer in this.answers) {
@@ -99,6 +146,7 @@ export class EvalSersaComponent implements OnInit, OnDestroy {
     }
     return risk;
   }
+  /*
   pushAnswers(risk) {
     let evaluations = this.getFormService.getEvaluations(this.infrastructureId);
     var currentdate = new Date();
@@ -115,7 +163,7 @@ export class EvalSersaComponent implements OnInit, OnDestroy {
       "riskCalculated":risk
     }
     evaluations.push(answer);
-  }
+  }*/
 
 
   ngOnDestroy(): void {
@@ -123,8 +171,10 @@ export class EvalSersaComponent implements OnInit, OnDestroy {
   }
 
   goBack() {
-    this.router.navigate(['/' + this.type + 'Details/' + this.infrastructureId]);
+    this._location.back();
+    //this.router.navigate(['/' + this.type + 'Details/' + this.infrastructureId]);
   }
+
   arrayComplete(pArray): Boolean {
     for (var i = 1; i < pArray.length; i++) {
       if (pArray[i] == null) {
@@ -134,12 +184,16 @@ export class EvalSersaComponent implements OnInit, OnDestroy {
     return true;
   }
 
+ 
+
   popErrorToast(pMessage: string) {
     var toast = {
       type: 'error',
       title: pMessage
     };
     this.toasterService.pop(toast);
+
+
   }
 
   popSuccessToast(pMesage: string) {
@@ -148,6 +202,32 @@ export class EvalSersaComponent implements OnInit, OnDestroy {
       title: pMesage
     };
     this.toasterService.pop(toast);
+  }
+
+  guardar(){
+    console.log(this.tomaDatosInfra);
+    //this.angularFireService.updateTomaDatos()
+    console.log("holaaa");
+
+    var aux = this.tomaDatosInfra.infraestructuras[this.idToma];
+
+    aux.res1 = ''+(this.answers[0] ? 0 : 1);
+    aux.res2 = ''+(this.answers[1] ? 0 : 1);
+    aux.res3 = ''+(this.answers[2] ? 0 : 1);
+    aux.res4 = ''+(this.answers[3] ? 0 : 1);
+    aux.res5 = ''+(this.answers[4] ? 0 : 1);
+    aux.res6 = ''+(this.answers[5] ? 0 : 1);
+    aux.res7 = ''+(this.answers[6] ? 0 : 1);
+    aux.res8 = ''+(this.answers[7] ? 0 : 1);
+    aux.res9 = ''+(this.answers[8] ? 0 : 1);
+    aux.res10 = ''+(this.answers[9] ? 0 : 1);
+
+    this.tomaDatosInfra.infraestructuras[this.idToma] = aux;
+
+    console.log("adiooooos");
+    console.log(this.tomaDatosInfra);
+    this.angularFireService.updateTomaDatos(this.tomaDatosInfra, this.tomaDatosId);
+
   }
 
 }
